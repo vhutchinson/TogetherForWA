164cf2bce49f5de6c2f7751b0b111d6292a10311
vhutchinson <virginiaehutchinson@gmail.com>
2019-07-27 18:07:34 -0500

    Added favicon, main index page, navigation including side nav bar and footer.

---
+++ html/index.php
@@ -0,0 +1,15 @@
+<?php include './navigation.php';?>
+
+<body>
+	<div class=Index-gallery-item-content-body>
+		<p>
+			<strong>Together</strong>
+		</p>
+		<p>
+			<strong>For</strong>
+		</p>
+		<p>
+			<strong>West Africa</strong>
+		</p>
+	</div>
+</body>
--- html/navbar.php
+++ html/navbar.php
@@ -2,11 +2,14 @@
 <html lang="en">
 <?php
 session_start(); 
-include(__DIR__ .'./../../php/dbconnect.php');
+include(__DIR__ .'\..\php\dbconnect.php');
 ?>
 <head>
   <title>Together For West Africa</title>
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="..\styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap" rel="stylesheet">
+  <link rel="icon" href="../Logo.ico" type="image/x-icon" />
+  <link rel="shortcut icon" href="../Logo.ico" type="image/x-icon" />
 </head>
 <nav class="sidenav">
   <a href="#">Home</a>
