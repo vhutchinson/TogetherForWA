312729356eb8bf96f9ffaeeaa47d00627035f1a2
vhutchinson <virginiaehutchinson@gmail.com>
2019-07-28 19:20:45 -0500

    Added stylized title, icon links to photo album pages.

--- html/index.php
+++ html/index.php
@@ -1,15 +1,15 @@
 <?php include 'navigation.php';?>
 
 <body>
-	<div class=Index-gallery-item-content-body>
-		<p>
-			<strong>Together</strong>
-		</p>
-		<p>
-			<strong>For</strong>
-		</p>
-		<p>
-			<strong>West Africa</strong>
-		</p>
+	<div class = main>
+		<p id="first" class="fadeInAnimated">Together</p>
+		<p id="second" class="fadeInAnimated">For</p>
+		<p id="third" class="fadeInAnimated">West Africa</p>
+	</div>
+	<div class="fadeInAnimated" id=fourth>
+			<div class="tooltip" id=svg_container1><a href="art.php"><?php echo file_get_contents("../Images/Icons/ios-brush.svg"); ?></a><span class="tooltiptext">Art</span></div>
+			<div class="tooltip" id=svg_container2><a href="wildlife.php"><?php echo file_get_contents("../Images/Icons/ios-bug.svg"); ?></a><span class="tooltiptext">Wildlife</span></div>
+			<div class="tooltip" id=svg_container3><a href="food.php"><?php echo file_get_contents("../Images/Icons/ios-nutrition.svg"); ?></a><span class="tooltiptext">Food</span></div>
+			<div class="tooltip" id=svg_container4><a href="smiles.php"><?php echo file_get_contents("../Images/Icons/ios-happy.svg"); ?></a><span class="tooltiptext">Smiles</span></div>
 	</div>
 </body>
--- html/navbar.php
+++ html/navbar.php
@@ -7,7 +7,8 @@ include(__DIR__ .'\..\php\dbconnect.php');
 <head>
   <title>Together For West Africa</title>
   <link rel="stylesheet" href="..\styles.css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap" rel="stylesheet">
+  <link href='http://fonts.googleapis.com/css?family=Ubuntu:bold' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Vollkorn' rel="stylesheet">
   <link rel="icon" href="../Logo.ico" type="image/x-icon" />
   <link rel="shortcut icon" href="../Logo.ico" type="image/x-icon" />
 </head>
