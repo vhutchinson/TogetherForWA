cf679109aaf7355de3eececc6b54ded28aa6e5e2
vhutchinson <virginiaehutchinson@gmail.com>
2019-07-28 21:02:15 -0500

    Added content to contact page/form.

--- html/contact.php
+++ html/contact.php
@@ -1,6 +1,27 @@
 <?php include 'navigation.php';?>
 
 <body>
-	<div>
+	<h3>Tell Us What You Think</h3>
+
+	<div class="container">
+	<form action="../php/action form.php" method="post">
+		<label for="fname">First Name</label>
+		<input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+		<label for="lname">Last Name</label>
+		<input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+		<label for="country">Country</label>
+		<select id="country" name="country">
+		<option value="usa">USA</option>
+		<option value="australia">Australia</option>
+		<option value="canada">Canada</option>
+		</select>
+
+		<label for="subject">Subject</label>
+		<textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+		<input type="submit" value="Submit">
+	</form>
 	</div>
 </body>
---
+++ php/action form.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Thank you for your input, <?php echo $_POST["firstname"]; ?><br>
+You have submitted the following comment: <?php echo $_POST["subject"]; ?>
+
+</body>
+</html>
