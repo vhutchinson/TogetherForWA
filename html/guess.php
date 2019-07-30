9e8edd7aaec4282c266f2d8e5afad467e5cf2c90
vhutchinson <virginiaehutchinson@gmail.com>
2019-07-29 20:08:22 -0500

    Added images and reveal text for Guess page.

--- html/guess.php
+++ html/guess.php
@@ -1,6 +1,29 @@
 <?php include 'navigation.php';?>
 
 <body>
-	<div>
+	<h1>Can you guess what it is?</h1>
+	<div class="textOnGuessImage">
+		<img src="../Images/Guess/Adi.jpg" alt="Mancala">
+		<h3 class="centered">Adi</h3>
+		<h2 class="centered">Called Adi by the Ewe people in Togo, but also known as Mancala in many other parts of the world, this is the oldest known games to still be widely played today. Mancala is a generic name for a family of two-player turn-based strategy board games played with small stones, beans, or seeds and rows of holes or pits in the earth, a board or other playing surface. The objective is usually to capture all or some set of the opponent's pieces. Versions of the game date back to the 7th century.</h2>
 	</div>
+	<br><br>
+	<div class="textOnGuessImage">	
+		<img src="../Images/Guess/Coffee.jpg" alt="Cold Coffee">
+		<h3>Cold Coffee</h3>
+		<h2>Luna's iced mocha coffee in a can is very refreshing on hot days during the dry season!</h2>
+	</div>
+	<br><br>
+	<div class="textOnGuessImage">	
+		<img src="../Images/Guess/noni.jpg" alt="Noni">
+		<h3>Noni</h3>
+		<h2>Natively from Southeast Asia and Australia, this fruit has become popular in Togo. Instead of being eaten, it is strained once overly ripe. The juice this produces is believed to be very medicinal.</h2>
+	</div>
+	<br><br>
+	<div class="textOnGuessImage">	
+		<img src="../Images/Guess/hut.jpg" alt="Mud House">
+		<h3>Mud House</h3>
+		<h2>After this tied stick infrastructure is completed, mud is rubbed in by hand to form the walls of the house. It may not be as long-lasting as a cement house, but it stays much cooler than one.</h2>
+	</div>
+	<br><br><br><br>
 </body>
--- php/action form.php
+++ php/action form.php
@@ -3,6 +3,9 @@
 
 Thank you for your input, <?php echo $_POST["firstname"]; ?><br>
 You have submitted the following comment: <?php echo $_POST["subject"]; ?>
+<br>
+<br>
+<font color= "red"><a href="../html/index.php">Return to Home Page</a></font>
 
 </body>
 </html>
